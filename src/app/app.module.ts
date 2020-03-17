941c7052b381d15f4146b29013bba867b410e644
fjuribe <fjuribeui@gmail.com>
2020-03-17 13:55:51 -0300

    Iniciando proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { InformacionComponent } from './componentes/informacion/informacion.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InformacionComponent,
+
   ],
   imports: [
     BrowserModule,
